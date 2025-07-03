92104a2d41ee671dc6fcef41ee7b4f441f203630
almond1411 <mungden.lahpai@gmail.com>
2025-07-03 10:37:30 +0630

    Update Companies.sql

--- admin/Companies.sql
+++ admin/Companies.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 27, 2025 at 10:57 AM
+-- Generation Time: Jul 02, 2025 at 07:04 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,6 +43,14 @@ CREATE TABLE `Companies` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `Companies`
+--
+
+INSERT INTO `Companies` (`Id`, `Name`, `Type`, `Address1`, `Address2`, `City`, `Email`, `Website`, `Class`, `Link`, `Created`, `Updated`) VALUES
+(1, 'Travex', '1', '321, Padonar Street', 'Sanchaung', 'Yangon', 'info@travex-myanmar.com', 'www.travex.com', 'D', 'Com_68622f55e64103.39292758', '2025-06-30 06:31:49', '2025-06-30 06:31:49'),
+(2, 'Voyager', '1', '56, Pyay Road', 'Dagon', 'Yangon', 'info@voyage-myanmar.com', 'www.voyager.com', 'D', 'Com_68623473336ad0.10032060', '2025-06-30 06:53:39', '2025-06-30 06:53:39');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -60,7 +68,7 @@ ALTER TABLE `Companies`
 -- AUTO_INCREMENT for table `Companies`
 --
 ALTER TABLE `Companies`
-  MODIFY `Id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
