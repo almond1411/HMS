4890a0c1529b9b0f9c0a7abbcc4662211824d391
almond1411 <mungden.lahpai@gmail.com>
2025-07-08 00:19:50 +0630

    Select_Companies

--- admin/php/select_Companies.php
+++ admin/php/select_Companies.php
@@ -18,6 +18,7 @@ $rows_Companies = $db->Resultset();
             <th>Website</th>
             <th>Type</th>
             <th>Class</th>
+            <th>######</th>
         </tr>
     </thead>
     <tbody>
@@ -25,6 +26,27 @@ $rows_Companies = $db->Resultset();
         <tr>
             <td><?php echo $row_Companies->Id; ?></td>
             <td><?php echo $row_Companies->Name; ?></td>
+            <td><?php echo $row_Companies->City; ?></td>
+            <td><a href="<?php echo $row_Companies->Email; ?>"><?php echo $row_Companies->Email; ?></a></td>
+            <td><a href="<?php echo $row_Companies->Website; ?>"><?php echo $row_Companies->Website; ?></a></td>
+            <td>
+                <?php 
+                //getting data from the table Company_Type
+                $select_Company_Types = "SELECT * FROM Company_Types WHERE Id = :Id ;";
+                $db->prepare($select_Company_Types);
+                $db->Bind(":Id", $row_Companies->Type);
+                $rows_Company_Types = $db->Resultset();
+                foreach ($rows_Company_Types as $row_Company_Types) {
+                    echo $row_Company_Types->Type;
+                }
+                ?>
+            </td>
+            <td><?php echo $row_Companies->Class; ?></td>
+            <td>
+                <a href="view_Company.html?Companies_Link="><button type="button" class="small-button">View</button></a>
+                &nbsp;
+                <a href="edit_Company.html?Comapnies_Link="><button type="button" class="small-button">Edit</button></a>
+            </td>
         </tr>
     <?php endforeach; ?>    
     </tbody>
