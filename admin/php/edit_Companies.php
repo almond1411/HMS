b5cf9f318e6e9bb2300e8580809585c4df753efc
DenLahpai <31785293+DenLahpai@users.noreply.github.com>
2025-07-09 07:19:37 +0630

    working on edit_Companies.php

--- admin/php/edit_Companies.php
+++ admin/php/edit_Companies.php
@@ -9,5 +9,32 @@ $stm = "SELECT * FROM Companies WHERE Link = :CompaniesLink ;";
 $db->prepare($stm);
 $db->Bind(":CompaniesLink", $_POST['CompaniesLink']);
 $rows_Companies = $db->Resultset();
+foreach ($rows_Companies as $row_Companies) {
+    # code...
+}
 ?>
+<form id="myForm" action="#" method="post">
+    <!-- form-table -->
+    <div class="form-table">`
+        <table>
+            <thead></thead>
+            <tbody>
+                <tr>
+                    <td>Name: </td>
+                    <td><input type="text" name="Name" id="Name" value="<?php echo $row_Companies->Name; ?>"></td>
+                    <td>Type: </td>
+                    <td>
+                        <select name="Type">
+                        <?php 
+                        //getting data from the table Company_Types
+                        $get_Company_Types = "SELECT * FROM Company_Types ;";
 
+                        ?>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- form-table ends -->
+</form>
