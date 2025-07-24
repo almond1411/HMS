c6ba144cebae75d2908494d33374f0a1ebddc6cc
Almon1411 <mungden.lahpai@gmail.com>
2025-07-24 15:35:45 +0630

    d
    ds

--- admin/php/select_Company_Users.php
+++ admin/php/select_Company_Users.php
@@ -5,11 +5,40 @@ require_once "functions.php";
 
 if (isset($_POST['CompaniesLink'])) {
     $db = new database();
-    $select_Users = "SELECT * FROM Users WHERE CompaniesLink = :CompaniesLink ;";
-    $db->prepare($select_Users);
-    $db->Bind(":CompaniesLink", $_POST['CompaniesLink]);
-    $rows_Users = $db->Resultset();
-    foreach ($rows_Users as $row_Users):
+    $stm = "SELECT * FROM Users WHERE CompaniesLink = :CompaniesLink ;";
+    $db->prepare($stm);
+    $db->Bind(":CompaniesLink", $_POST['CompaniesLink']);
+    $rows_Users = $db->Resultset();   
 }
 ?>
-
+<table>
+    <thead>
+        <tr>
+            <th>Username</th>
+            <th>Title</th>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Department</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Status</th>
+            <th>Access</th>
+            <th>Created On</th>
+            <th>Remark</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($rows_Users as $row_Users): ?>
+        <tr>
+            <td><?php echo $row_Users->Username; ?></td>
+            <td><?php echo $row_Users->Title; ?></td>
+            <td><?php echo $row_Users->Name; ?></td>
+            <td><?php echo $row_Users->Position; ?></td>
+            <td><?php echo $row_Users->Department; ?></td>
+            <td><a href="mailto:<?php echo $row_Users->Email; ?>"><?php echo $row_Users->Email; ?></a></td>
+            <td><?php echo $row_Users->Phone; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php //endforeach; ?>
