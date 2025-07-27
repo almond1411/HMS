6686035da9f0ff3d3bd01e1804df3505cce7f4de
Almond1411 <mungden.lahpai@gmail.com>
2025-07-28 00:01:57 +0630

    Update Hotel.sql

--- admin/Hotel.sql
+++ admin/Hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 12, 2025 at 04:45 PM
+-- Generation Time: Jul 27, 2025 at 07:31 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -128,6 +128,7 @@ CREATE TABLE `Room_Types` (
 
 CREATE TABLE `Users` (
   `Id` int(6) NOT NULL,
+  `Link` varchar(240) NOT NULL,
   `Email` char(60) NOT NULL,
   `Password` varchar(240) NOT NULL,
   `Title` char(6) NOT NULL,
@@ -135,9 +136,18 @@ CREATE TABLE `Users` (
   `Position` char(30) NOT NULL,
   `CompaniesLink` varchar(240) NOT NULL,
   `Status` int(1) NOT NULL,
+  `Access` int(1) NOT NULL DEFAULT 1,
   `Created` timestamp NOT NULL DEFAULT current_timestamp(),
   `Updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`Id`, `Link`, `Email`, `Password`, `Title`, `Name`, `Position`, `CompaniesLink`, `Status`, `Access`, `Created`, `Updated`) VALUES
+(1, 'Usr_6884e4ce1f8797.37954958', 'denn.lahpai@gmail.com', '840d43278af24ac4d68dd2929228ec27f11c45ff8d07606abfadfcd2bdfc363494f5aab7b8cfbd567feabcae12a2cc8d5248c443054fdcd755e9e4b5325e13ca', 'Mr', 'Denisxx', 'Managing Director', 'Com_68623473336ad0.10032060', 1, 1, '2025-07-26 14:23:10', '2025-07-27 15:21:14'),
+(2, 'Usr_6884e9414dce86.72647834', 'den.lahpai@gmail.com', '840d43278af24ac4d68dd2929228ec27f11c45ff8d07606abfadfcd2bdfc363494f5aab7b8cfbd567feabcae12a2cc8d5248c443054fdcd755e9e4b5325e13ca', 'Mr', 'Denny', 'Tester', 'Com_68623473336ad0.10032060', 1, 1, '2025-07-26 14:42:09', '2025-07-27 11:47:42');
 
 --
 -- Indexes for dumped tables
@@ -193,7 +203,7 @@ ALTER TABLE `Company_Types`
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `Id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
